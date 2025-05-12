ee55b826b38c4fa974098dc7edfc4a510b382c8e
AndAv03 <andres.aviles@ucb.edu.bo>
2025-05-12 17:44:07 -0400

    primer commit

---
+++ src/test/java/bo/edu/ucb/AppTest.java
@@ -0,0 +1,20 @@
+package bo.edu.ucb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
